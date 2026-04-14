ad3eb26c79ab367cf3c284e67c93dbd0f4087de1
noahmoltzan <noahmoltzan@lumiluxelight.com>
2026-04-13 20:13:41 -0500

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -27,7 +27,6 @@ export default async function handler(req, res) {
               lastName: "${lastName}",
               emails: [{ address: "${email}" }],
               phones: [{ number: "${phone}" }],
-              notes: "Price: ${price}",
               billingAddress: {
                 street1: "${address}"
               }
